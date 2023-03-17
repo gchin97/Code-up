0ec24cad0024bfdf5e63ee3e029d0130feb10b8e
gchin97 <graceschinn@gmail.com>
2023-03-17 14:13:12 +0900

    ~94

--- 80~90.py
+++ 80~90.py
@@ -18,20 +18,56 @@ count = 0
 for x in range(r):
     for y in range(g):
         for z in range(b):
-            print("{},{},{}".format(x, y, z))
+            print("{} {} {}".format(x, y, z))
+            count += 1
 print(count)
 83
-
+h, b, c, s = map(int, input().split())
+mb = round((h * b * c * s / 8) / 1024 / 1024, 1)
+print('{} MB'.format(mb))
 84
-
+h, b, c = map(int, input().split())
+mb = round((h * b * c / 8) / 1024 / 1024, 2)
+print('{} MB'.format(mb))
 85
+h, b, c = map(int, input().split())
+mb = round((h * b * c / 8) / 1024 / 1024, 2)
+print('{:.2f} MB'.format(mb))
 
 86
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum = sum+i
+    i += 1
+    if (sum >= n):
+        break
+print(sum)
 
 87
+n = int(input())
+for i in range(1, n+1):
+    if i % 3 == 0:
+        continue
+    print(i, end=" ")
 
 88
+a, d, n = map(int, input().split())
+sum = a
+for i in range(a, a+n-1):
+    sum = sum+d
+print(sum)
 
 89
+a, d, n = map(int, input().split())
+sum = a
+for i in range(a, a+n-1):
+    sum *= d
+print(sum)
 
 90
+a, m, d, n = map(int, input().split())
+sum = a
+for i in range(a, a+n-1):
+    sum = sum*m+d
+print(sum)
---
+++ 90~100.py
@@ -0,0 +1,36 @@
+from random import randint
+91
+a, b, c = map(int, input().split())
+n = 1
+while n % a != 0 or n % b != 0 or n % c != 0:
+    n += 1
+print(n)
+92
+n = int(input())
+temp = [0] * 23
+nums = input().split()
+for i in nums:
+    temp[int(i)-1] += 1
+for i in temp:
+    print(i, end=' ')
+93
+n = int(input())
+nums = input().split()
+nums.reverse()
+for i in nums:
+    print(int(i), end=" ")
+
+n = int(input())
+nums = input().split()
+nums.reverse()
+for i in nums:
+    print(int(i), end=' ')
+94
+
+95
+
+96
+
+97
+
+98
