73f31fbde278c0fceefe59be81143b3c1db3b1da
gchin97 <graceschinn@gmail.com>
2023-03-17 13:28:32 +0900

    ~90

--- 80~90.py
+++ 80~90.py
@@ -1,9 +1,25 @@
 80
+n = input()
+for i in range(1, 16):
+    print('%x*%x=%x'.upper() %
+          (int(n, 16), int(hex(i), 16), (int(n, 16) * int(hex(i), 16))))
 
 81
+n = int(input())
+for i in range(1, n+1):
+    if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("X", end=" ")
+    else:
+        print(i, end=" ")
 
 82
-
+r, g, b = map(int, input().split())
+count = 0
+for x in range(r):
+    for y in range(g):
+        for z in range(b):
+            print("{},{},{}".format(x, y, z))
+print(count)
 83
 
 84
