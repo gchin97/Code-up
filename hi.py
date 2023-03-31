208b39aa435cc1cee0cb73782c5af9ebac24dc8f
gchin97 <graceschinn@gmail.com>
2023-03-31 22:12:43 +0900

    a

--- hi.py
+++ hi.py
@@ -1,5 +1,6 @@
 with open("EmployeeDemographic.csv", "w") as report_file:
-    report_file.write("'EmployeeID', 'FirstName', 'LastName', 'Age', 'Gender'")
+    report_file.write(
+        "'EmployeeID', 'FirstName', 'LastName', 'Age', 'Gender'")
     report_file.write("\n1001, 'Jim', 'Halpert', 30, 'Male'")
     report_file.write("\n1002, 'Pam', 'Beasley', 30, 'Female'")
     report_file.write("\n1003, 'Dwight', 'Schrute', 29, 'Male'")
@@ -9,6 +10,13 @@ with open("EmployeeDemographic.csv", "w") as report_file:
     report_file.write("\n1007, 'Meredith', 'Palmer', 32, 'Female'")
     report_file.write("\n1008, 'Stanley', 'Hudson', 38, 'Male'")
     report_file.write("\n1009, 'Kevin', 'Malone', 31, 'Male'")
+
+with open("EmployeeDemographic.csv", "r") as report_file:
+    filedata = report_file.read()
+filedata = filedata.replace("'", "")
+
+# with open("EmployeeDemographic.csv", "w") as report_file:
+#     report_file(filedata)
 
 with open("EmployeeSalary.csv", "w") as report_files:
     report_files.write("'EmployeeID', 'JobTitle', 'Salary'")
@@ -21,3 +29,10 @@ with open("EmployeeSalary.csv", "w") as report_files:
     report_files.write("\n1007, 'Supplier Relations', 41000")
     report_files.write("\n1008, 'Salesman', 48000")
     report_files.write("\n1009, 'Accountant', 42000")
+
+with open("EmployeeSalary.csv", "r") as report_files:
+    filesdata = report_files.read()
+filesdata = filesdata.replace("'", "")
+
+with open('EmployeeSalary.csv', 'w') as report_files:
+    report_files.write(filesdata)
