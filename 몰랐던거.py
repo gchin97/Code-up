901cc9bda78ff2d62bbf864d715691a648224cf5
Summerolie <81975861+gchin97@users.noreply.github.com>
2023-06-12 10:24:38 +0900

    Update 몰랐던거.py

--- 몰랐던거.py
+++ 몰랐던거.py
@@ -26,3 +26,33 @@
 #     if t == 10:
 #         print("나무 끝")
 #         breakㅁ
+
+numpy 
+1. 다차원 배열 생성 방법
+- np.array(리스트), np.array(중첩 ㅣ스트)
+- np.zeros, np.ones, np.empty, np.full
+- 랜덤 함수 이용하기(np.random.함수)
+- np.linspace
+2. 속성
+- ndim(차원 정보)
+- shape(모양 볼 수 있음)
+- dtype(data type, 정수 np.int32, 실수 np.float64, 문자열 np.string/str
+3. 추가 삽입 색인
+- np. append, np. insert, np.delete
+4. 색인
+- indexing (순방향, 역방향)
+- 슬라이싱(np.s_[:])
+- fancy 색인: 정수형 배열 색인 => 변수[[idx, idx]]
+
+x = [1,2,3]
+x2 = x #얕은 복사(주소값 복사), 따라서 x2에서 값을 변경하면 x 도 변경이 됨 
+x1 = x2 함수를 사용하지 않고 주소를 복사해서 넌 너고 난 나야 x2 변경했을때 x1도 변경됨 
+
+- python의 깊은 복사 3가지 방법 (깊은 복사이기 때문에 x3에서 값을 변경해도 원본인 x는 영향받지 않음)
+x3 = x[:]
+x3 = x.copy(x)
+x3 = list(x) 
+
+- numpy는 [:] 슬라이싱  처리는 얕은 복사로 처리
+
+
